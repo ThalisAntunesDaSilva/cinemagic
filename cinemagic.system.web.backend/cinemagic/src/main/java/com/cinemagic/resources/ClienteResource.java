cd04444da04fbe4765cd71373a225083a5c48d48
gabrielcampos12 <gabrielcamposfreitas12@gmail.com>
2021-02-10 14:04:38 -0300

    criada estrutura da ClienteResource

---
+++ cinemagic.system.web.backend/cinemagic/src/main/java/com/cinemagic/resources/ClienteResource.java
@@ -0,0 +1,26 @@
+package com.cinemagic.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cinemagic.domain.Cliente;
+
+@RestController(value = "/clientes")
+public class ClienteResource {
+
+	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
+	public ResponseEntity<Cliente> findById(@PathVariable Integer id){
+		return null;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Cliente cliente){
+		return null;
+	}
+	
+	
+}
