380f990b2682b066373d0038ef987f882e21d263
gabrielcampos12 <gabrielcamposfreitas12@gmail.com>
2021-02-23 15:50:30 -0300

    implementado models aplicacao mobile

--- cinemagic.system.mobile.fidelidade/cinemagic/lib/main.dart
+++ cinemagic.system.mobile.fidelidade/cinemagic/lib/main.dart
@@ -1,20 +1,9 @@
+import 'package:cinemagic/app/app_module.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Cinemagic'),
-    );
-  }
+  runApp(ModularApp(
+    module: AppModule(),
+  ));
 }
