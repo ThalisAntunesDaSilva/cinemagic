e9253f4963484aa4de4694c3753009ebe5e1dd2e
Matheus Lima <matheus.zzalamena@gmail.com>
2021-02-26 15:15:30 -0300

    Sugestao 0.1
    Criação das classes domain Sugestão, SugestaoRepository e SugestaoResource, para implementaçao da funcionalidade envio de sugestões pelos clientes

---
+++ cinemagic.system.web.backend/cinemagic/src/main/java/com/cinemagic/resources/SugestaoResource.java
@@ -0,0 +1,44 @@
+package com.cinemagic.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cinemagic.domain.Sugestao;
+import com.cinemagic.repositories.SugestaoRepository;
+
+/*
+ * Essa classe trabalha como controller, vai receber as requisiçoes HTTP
+ */
+@RestController
+@RequestMapping(value = "/sugestao")
+public class SugestaoResource {
+
+	//criar um ponto de ingesao do nosso repository 
+	@Autowired
+	SugestaoRepository repo;
+	
+	@GetMapping("/sugestoes")
+	public List<Sugestao> listaSugestoes(){
+		return repo.findAll();
+		
+	}
+	
+	@GetMapping("/sugestao/{id}")
+	public Sugestao sugestaoUnica(@PathVariable(value = "id") int id) {
+		return repo.findById(id);
+		
+	}
+	
+	@PostMapping
+	public Sugestao salvarSugestao(@RequestBody Sugestao sugestao) {
+		return repo.save(sugestao);
+	}
+	
+}
