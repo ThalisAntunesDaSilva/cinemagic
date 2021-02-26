414e0242674ee33e65075b918f799026576d40f9
Matheus Lima <matheus.zzalamena@gmail.com>
2021-02-26 15:45:08 -0300

    Sugestao 0.2
    Métodos de atualizar e deletar implementados

--- cinemagic.system.web.backend/cinemagic/src/main/java/com/cinemagic/resources/SugestaoResource.java
+++ cinemagic.system.web.backend/cinemagic/src/main/java/com/cinemagic/resources/SugestaoResource.java
@@ -3,9 +3,11 @@ package com.cinemagic.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,8 +38,18 @@ public class SugestaoResource {
 		
 	}
 	
-	@PostMapping
+	@PostMapping("/sugestao")
 	public Sugestao salvarSugestao(@RequestBody Sugestao sugestao) {
+		return repo.save(sugestao);
+	}
+	
+	@DeleteMapping("/sugestao")
+	public void deletarSugestao(@RequestBody Sugestao sugestao) {
+		repo.delete(sugestao);
+	}
+	
+	@PutMapping("/sugestao")
+	public Sugestao atualizarSugestao(@RequestBody Sugestao sugestao) {
 		return repo.save(sugestao);
 	}
 	
