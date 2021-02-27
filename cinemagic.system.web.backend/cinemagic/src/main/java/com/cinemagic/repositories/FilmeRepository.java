a02cdcf765573815be464dbcfbe88f73a77306bd
Matheus Lima <matheus.zzalamena@gmail.com>
2021-02-27 01:57:22 -0300

    CRUD filmes 0.1
    Salvar, deletar, buscar, atualizar.

--- cinemagic.system.web.backend/cinemagic/src/main/java/com/cinemagic/repositories/FilmeRepository.java
+++ cinemagic.system.web.backend/cinemagic/src/main/java/com/cinemagic/repositories/FilmeRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cinemagic.domain.Filme;
+
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Integer>{
 
+	//metodo que procura uma sugestao apenas pelo id
+	Filme findById(int id);
 }
--- cinemagic.system.web.backend/cinemagic/src/main/java/com/cinemagic/resources/FilmeResource.java
+++ cinemagic.system.web.backend/cinemagic/src/main/java/com/cinemagic/resources/FilmeResource.java
@@ -3,7 +3,11 @@ package com.cinemagic.resources;
 import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,7 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.cinemagic.domain.Filme;
 import com.cinemagic.dto.FilmeDTO;
+import com.cinemagic.repositories.FilmeRepository;
 import com.cinemagic.services.FilmeService;
+
+import java.util.List;
 
 @RestController()
 @RequestMapping(value = "/filmes")
@@ -19,6 +26,7 @@ public class FilmeResource {
 
 	@Autowired
 	FilmeService filmeService;
+	FilmeRepository repo;
 	
 	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
 	public ResponseEntity<Filme> findById(@PathVariable Integer id){
@@ -38,6 +46,31 @@ public class FilmeResource {
 		URI url = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId())
 				.toUri();
 		return ResponseEntity.created(url).build();
+	}
+	
+	@GetMapping("/filmes")
+	public List<Filme> listarFilmes(){
+		return repo.findAll();
+	}
+	
+	@GetMapping("/filme/{id}")
+	public Filme buscaFilme(@PathVariable(value = "id") int id) {
+		return repo.findById(id);
+	}
+	
+	@PostMapping("/filme")
+	public Filme salvarFilme(@RequestBody Filme filme) {
+		return repo.save(filme);
+	}
+	
+	@DeleteMapping("/filme")
+	public void deletarFilme(@RequestBody Filme filme) {
+		repo.delete(filme);
+	}
+	
+	@PutMapping("/sugestao")
+	public Filme atualizarFilme(@RequestBody Filme filme) {
+		return repo.save(filme);
 	}
 	
 }
