05f9edf22bd78e84c05c3e95dc88502dcd06f8c6
Ketrin Vargas <ketrin_vargas@outlook.com>
2021-04-06 09:17:38 -0300

    Page CadastroCinema

--- cinemagic.system.web.front/develop/src/components/ferramentasCinema/index.js
+++ cinemagic.system.web.front/develop/src/components/ferramentasCinema/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import cinemagic from '../../assets/logo.png';
+import cinemagic from '../../assets/cinemagic.jpeg';
 import './styles.css';
 
 
@@ -9,7 +9,7 @@ const Example = (props) => {
     return (
         <div className="home-container">
 
-            <section className="header col-xs-15 col-sm-12 col-md-15 col-lg-12">
+            <section className="header col-xs-110 col-sm-90 col-md-110 col-lg-90">
 
                 <div className="header-centro">
                     <img src={cinemagic} className="header-centro" alt="cinemagic" />
